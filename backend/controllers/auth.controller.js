a08624f474e2609aa23f1c8b758b8ebc7092bfbb
umaan_74 <umanlandge74@gmail.com>
2026-09-18 16:39:31 +0530

    auth Controllers done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import Session from "../models/session.model.js";
 
+// Register Function Logic
 export async function Register(req, res) {
   const { username, email, password } = req.body;
 
@@ -13,7 +14,7 @@ export async function Register(req, res) {
     return res.json("User Already Exist !");
   }
   const hashedPassword = await bcrypt.hash(password, 10);
-  const user = new User({
+  const user = await User({
     username: username,
     email: email,
     password: hashedPassword,
@@ -24,6 +25,7 @@ export async function Register(req, res) {
     user,
   });
 }
+// Login Function Logic
 export async function Login(req, res) {
   const { username, password } = req.body;
   const user = await User.findOne({
@@ -40,6 +42,7 @@ export async function Login(req, res) {
   const AccessToken = jwt.sign(
     {
       username,
+      userId: user._id,
     },
     process.env.JWT_SECRET_KEY,
     {
@@ -49,6 +52,7 @@ export async function Login(req, res) {
   const RefreshToken = jwt.sign(
     {
       username,
+      userId: user._id,
     },
     process.env.JWT_SECRET_KEY,
     {
@@ -71,3 +75,122 @@ export async function Login(req, res) {
   });
 }
 
+// Refresh function logic (Refresh Token Rotation)
+export async function Refresh(req, res) {
+  const RefreshToken = req.cookies.refreshToken;
+  const decoded = jwt.verify(RefreshToken, process.env.JWT_SECRET_KEY);
+  const user = decoded;
+
+  const Sessions = await Session.find({
+    userId: user.userId,
+     revoked: false,
+  });
+  let userSession = null;
+  for (const s of Sessions) {
+    const isRefreshToken_Valid = await bcrypt.compare(
+      RefreshToken,
+      s.refreshTokenHash,
+    );
+    if (isRefreshToken_Valid) {
+      userSession = s;
+      break;
+    }
+  }
+  if (userSession === null) {
+    return res.json({
+      message: "Refresh Token is not Valid",
+    });
+  }
+
+  const NewAccessToken = jwt.sign(
+    {
+      username: user.username,
+      userId: user.userId,
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "15m",
+    },
+  );
+
+  const NewRefreshToken = jwt.sign(
+    {
+      username: user.username,
+      userId: user.userId,
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "7d",
+    },
+  );
+
+  const NewRefreshTokenHash = await bcrypt.hash(NewRefreshToken, 10);
+  userSession.refreshTokenHash = NewRefreshTokenHash;
+  await userSession.save();
+
+  res.json({
+    message: "New Access and Refresh Token is Generated",
+    NewAccessToken,
+  });
+}
+
+// Logout function logic
+export async function Logout(req, res) {
+  const RefreshToken = req.cookies.refreshToken;
+  const decoded = jwt.verify(RefreshToken, process.env.JWT_SECRET_KEY);
+  const user = decoded;
+
+  const Sessions = await Session.find({
+    userId: user.userId,
+    revoked: false,
+  });
+  let userSession = null;
+  for (const s of Sessions) {
+    const isRefreshToken_Valid = await bcrypt.compare(
+      RefreshToken,
+      s.refreshTokenHash,
+    );
+    if (isRefreshToken_Valid) {
+      userSession = s;
+      break;
+    }
+  }
+  if (userSession === null) {
+    return res.json({
+      message: "Refresh Token is not Valid",
+    });
+  }
+  userSession.revoked = true;
+  await userSession.save();
+
+  res.clearCookie("refreshToken");
+  res.json({
+    message: "User Logout Successfully",
+  });
+}
+// Logout-all function Logic
+export async function LogoutAll(req, res) {
+  const RefreshToken = req.cookies.refreshToken;
+  const decoded = jwt.verify(RefreshToken, process.env.JWT_SECRET_KEY);
+  const user = decoded;
+
+  const Sessions = await Session.find({
+    userId: user.userId,
+    revoked: false,
+  });
+
+  for (const s of Sessions) {
+    s.revoked = true;
+    await s.save();
+  }
+  if (Sessions.length === 0) {
+    return res.json({
+      message: "Refresh Token is not Valid",
+    });
+  }
+
+  res.clearCookie("refreshToken");
+  res.json({
+    message: "Logout from All devices Successfully",
+  });
+}
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { Login, Register } from '../controllers/auth.controller.js';
+import { Login, Logout, LogoutAll, Refresh, Register } from '../controllers/auth.controller.js';
 
 const authRouter=express.Router();
 
 authRouter.post("/register",Register)
 authRouter.post("/login",Login)
-// authRouter.post("/refresh",)
-// authRouter.post("/logout",)
-// authRouter.post("/logout-all",)
+authRouter.post("/refresh",Refresh)
+authRouter.post("/logout",Logout)
+authRouter.post("/logout-all",LogoutAll)
 
 export default authRouter;
